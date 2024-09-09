c120d4d2b0a0c27ad081a2b503137e73e6e9bb35
MatanLank <matanlankri8@gmail.com>
2024-09-09 19:15:03 +0300

    add github webhook

--- main.tf
+++ main.tf
@@ -37,3 +37,53 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
 output "lambda_function_name" {
   value = aws_lambda_function.example.function_name
 }
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "example-api"
+  description = "API for Lambda function"
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "webhook"
+}
+
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.root.id
+  http_method             = aws_api_gateway_method.post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-west-2:lambda:path/2015-03-31/functions/${aws_lambda_function.example.arn}/invocations"
+}
+
+resource "aws_api_gateway_deployment" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+  ]
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The source ARN is the ARN of the API Gateway stage.
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  value = "${aws_api_gateway_deployment.api.invoke_url}/webhook"
+}
